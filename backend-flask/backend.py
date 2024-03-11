3906751ae25c2e93b2a90b92b504a4e80e57e491
Suhail Khan <78406567+suhailkhan21@users.noreply.github.com>
2024-03-11 23:52:47 +0530

    Update backend.py

--- backend-flask/backend.py
+++ backend-flask/backend.py
@@ -5,10 +5,10 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST']='172.22.0.2'
-app.config['MYSQL_USER']='root'
-app.config["MYSQL_PASSWORD"]=''
-app.config["MYSQL_DB"]='flaskapp'
+app.config['MYSQL_HOST']='mysql'
+app.config['MYSQL_USER']='admin'
+app.config["MYSQL_PASSWORD"]='admin'
+app.config["MYSQL_DB"]='myDb'
 
 mysql=MySQL(app)
 CORS(app)
